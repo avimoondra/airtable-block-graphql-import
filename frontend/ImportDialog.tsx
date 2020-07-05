4394f83e1641b538184d31a848311f077c8b859d
Avi Moondra <avimoondra@gmail.com>
2020-07-05 15:32:24 -0700

    functional w/globalConfig sync for url + localStorage sync for headers

--- frontend/ImportDialog.tsx
+++ frontend/ImportDialog.tsx
@@ -1,25 +1,82 @@
+import { RecordDef } from "@airtable/blocks/dist/types/src/types/record";
 import {
   Box,
   Button,
   Dialog,
   FormField,
   Heading,
+  Icon,
   Input,
+  RecordCardList,
+  useBase,
 } from "@airtable/blocks/ui";
 import { JSONPath } from "jsonpath-plus";
+import { isEmpty } from "lodash";
 import React from "react";
 import { headerListToObj } from "./header-util";
+import {
+  findOrCreateTables,
+  insertQueryIntoQueriesTable,
+  insertRecordsIntoTables,
+  previewRecords,
+  previewSchema,
+  TableStatus,
+} from "./table-util";
 
 function ImportDialog(props: {
   onClose: () => void;
   url: string;
   headers: Array<{ key: string; value: string }>;
 }) {
+  const base = useBase();
   const [query, setQuery] = React.useState("");
   const [jsonPath, setJsonPath] = React.useState("");
   const [queryResponse, setQueryResponse] = React.useState({});
+  const [previewTables, setPreviewTables] = React.useState<
+    Map<string, Array<any>>
+  >();
+  const [previewTableStatuses, setPreviewTableStatuses] = React.useState<{
+    [tableName: string]: TableStatus;
+  }>();
+  const [recordDefsByTable, setRecordDefsByTable] = React.useState<
+    Map<string, Array<RecordDef>>
+  >();
 
-  // const { JSONPath } = require("jsonpath-plus");
+  let previewTableDivs = [];
+  if (previewTables && previewTableStatuses) {
+    previewTables.forEach((tableFields, tableName, _map) => {
+      let icon;
+      if (previewTableStatuses[tableName] === TableStatus.create) {
+        icon = <Icon name="grid"></Icon>;
+      } else if (previewTableStatuses[tableName] === TableStatus.modify) {
+        icon = <Icon name="warning"></Icon>;
+      } else if (previewTableStatuses[tableName] === TableStatus.match) {
+        icon = <Icon name="check"></Icon>;
+      }
+      previewTableDivs.push(
+        <div>
+          table name: {tableName}, {icon}
+          <div>{tableFields.map((field) => field.name)}</div>
+        </div>
+      );
+    });
+    console.log(previewTables);
+  }
+
+  let recordCardListByTable = new Map();
+  if (recordDefsByTable) {
+    recordDefsByTable.forEach((recordDefs, tableName, map) => {
+      const table = base.getTableByName(tableName);
+      recordCardListByTable.set(
+        table,
+        <RecordCardList
+          width={"200px"}
+          records={recordDefs}
+          fields={table.fields}
+        ></RecordCardList>
+      );
+    });
+  }
 
   return (
     <Dialog onClose={props.onClose} height={"90vh"}>
@@ -65,9 +122,55 @@ function ImportDialog(props: {
         </Button>
       </Box>
 
-      <Box height={"200px"} overflowY="auto" border={"1px solid gray"}>
-        <pre>{JSON.stringify(queryResponse, null, 2)}</pre>
+      <Box
+        height={"200px"}
+        overflowY="auto"
+        border={"1px solid lightgray"}
+        borderRadius="3px"
+      >
+        <pre style={{ margin: "0px", padding: "8px" }}>
+          {isEmpty(queryResponse) ? "" : JSON.stringify(queryResponse, null, 2)}
+        </pre>
       </Box>
+      <Box>{previewTableDivs}</Box>
+
+      <Button
+        variant="primary"
+        onClick={async () => {
+          await findOrCreateTables(base, queryResponse);
+          const { tables, tableStatuses } = previewSchema(base, queryResponse);
+          setPreviewTableStatuses(tableStatuses);
+          setPreviewTables(tables);
+        }}
+        disabled={isEmpty(queryResponse)}
+      >
+        Confirm Schema
+      </Button>
+
+      <Button
+        onClick={async () => {
+          const recordDefsByTable = await previewRecords(base, queryResponse);
+          console.log(recordDefsByTable);
+          setRecordDefsByTable(recordDefsByTable);
+        }}
+      >
+        Preview Data
+      </Button>
+
+      <Box display="flex" flexDirection="row" height="300px">
+        {Array.from(recordCardListByTable).map(([key, value]) => {
+          return value;
+        })}
+      </Box>
+
+      <Button
+        onClick={() => {
+          insertRecordsIntoTables(base, queryResponse);
+          insertQueryIntoQueriesTable(base, query);
+        }}
+      >
+        Import
+      </Button>
     </Dialog>
   );
 }
--- frontend/table-util.ts
+++ frontend/table-util.ts
@@ -1,70 +1,180 @@
-import { Base, FieldType } from "@airtable/blocks/models";
-import { isArray, keys } from "lodash";
-import { javascriptTypeToAirtableFieldType } from "./typeMapping";
+import { RecordDef } from "@airtable/blocks/dist/types/src/types/record";
+import { Base, FieldType, Table } from "@airtable/blocks/models";
+import { difference, isArray, keys, reject } from "lodash";
 
-export const createOrOverwriteTable = async (base: Base, json, key) => {
-  console.log(json[key]);
-  if (isArray(json[key])) {
-    const tableName = key;
-    const sampleObj = json[key][0];
-    const tableFields = keys(sampleObj).map((key) => {
-      const type = javascriptTypeToAirtableFieldType(sampleObj[key]);
-      return {
-        name: key,
-        type: type,
-        options:
-          type === FieldType.CHECKBOX
-            ? { icon: "check", color: "greenBright" }
-            : undefined,
-      };
-    });
-
-    console.log("createOrOverwriteTable");
-    // console.log(sampleObj);
-    // console.log(tableFields);
-
-    if (base.unstable_hasPermissionToCreateTable(tableName, tableFields)) {
-      let table = base.getTableByNameIfExists(tableName);
-      // if (
-      //   xor(
-      //     table.fields.map((field) => field.name),
-      //     tableFields.map((field) => field.name)
-      //   ).length !== 0
-      // ) {
-      //   throw Error("Delete the table!");
-      // }
-      if (!table) {
-        table = await base.unstable_createTableAsync(tableName, tableFields);
-      }
-
-      if (table.checkPermissionsForCreateRecords()) {
-        console.log(key);
-        const records = json[key].map((item) => {
-          const fields = {};
-          console.log(keys(item));
-          tableFields.forEach((tableField) => {
-            fields[tableField.name] = item[tableField.name];
-          });
-          return {
-            fields,
-          };
+const getTablesFromJsonStructure = (
+  json,
+  tables: Map<string, Array<any>>,
+  parentTable: string
+) => {
+  keys(json).forEach((key) => {
+    if (isArray(json[key])) {
+      // TODO handle object
+      const nonArrayKeys = reject(keys(json[key][0]), (_key) => {
+        return isArray(json[key][0][_key]);
+      });
+      const fields = [];
+      nonArrayKeys.forEach((_key) => {
+        fields.push({
+          name: _key,
+          type: FieldType.SINGLE_LINE_TEXT,
         });
-        console.log(records);
-        await table.createRecordsAsync(records);
+      });
+      // Creating or updating `MULTIPLE_RECORD_LINKS` fields is not supported.
+      // under https://airtable.com/developers/blocks/api/FieldType#MULTIPLE_RECORD_LINKS
+      if (parentTable) {
+        fields.push({
+          name: `${parentTable}_id`,
+          type: FieldType.MULTIPLE_RECORD_LINKS,
+        });
       }
+      tables.set(key, fields);
+      getTablesFromJsonStructure(json[key][0], tables, null);
     }
-  }
+  });
 };
 
-// const BATCH_SIZE = 50;
-// async function deleteAllRecords(table) {
-//   const records = useRecords(table);
-//   let i = 0;
-//   while (i < records.length) {
-//     const recordBatch = records.slice(i, i + BATCH_SIZE);
-//     // awaiting the delete means that next batch won't be deleted until the current
-//     // batch has been fully deleted, keeping you under the rate limit
-//     await table.deleteRecordsAsync(recordBatch);
-//     i += BATCH_SIZE;
-//   }
-// }
+export enum TableStatus {
+  "create",
+  "modify",
+  "match",
+}
+
+export const previewSchema = (base: Base, json) => {
+  const _tables: Map<string, Array<any>> = new Map<string, Array<any>>();
+  getTablesFromJsonStructure(json, _tables, null);
+  const _tableStatuses: { [tableName: string]: TableStatus } = {};
+  _tables.forEach(async (tableFields, tableName, _map) => {
+    let table = base.getTableByNameIfExists(tableName);
+    if (!table) {
+      _tableStatuses[tableName] = TableStatus.create;
+    } else if (
+      table &&
+      difference(
+        tableFields.map((field) => field.name),
+        table.fields.map((field) => field.name)
+      ).length !== 0
+    ) {
+      _tableStatuses[tableName] = TableStatus.modify;
+    } else {
+      _tableStatuses[tableName] = TableStatus.match;
+    }
+  });
+
+  return {
+    tables: _tables,
+    tableStatuses: _tableStatuses,
+  };
+};
+
+export const findOrCreateTables = async (base: Base, json) => {
+  const _tables: Map<string, Array<any>> = new Map<string, Array<any>>();
+  getTablesFromJsonStructure(json, _tables, null);
+  const tables: Array<Table> = [];
+  _tables.forEach(async (tableFields, tableName, _map) => {
+    let table = base.getTableByNameIfExists(tableName);
+    if (!table) {
+      if (base.unstable_hasPermissionToCreateTable()) {
+        table = await base.unstable_createTableAsync(tableName, tableFields);
+      }
+    }
+    tables.push(table);
+  });
+  return tables;
+};
+
+const getRecordDefsByTableFromJsonStructure = (
+  json,
+  tables: Array<Table>,
+  recordDefsByTable: Map<string, Array<RecordDef>>
+) => {
+  keys(json).forEach((key) => {
+    if (isArray(json[key])) {
+      const tableName = key;
+      const table = tables.find((table) => table.name === tableName);
+      json[tableName].forEach((item) => {
+        const newRecord: RecordDef = {};
+        table.fields.forEach((field) => {
+          newRecord[field.id] = item[field.name];
+        });
+
+        if (!recordDefsByTable.get(tableName)) {
+          recordDefsByTable.set(tableName, []);
+        }
+        const recordDefs = recordDefsByTable.get(tableName);
+        recordDefs.push(newRecord);
+        recordDefsByTable.set(tableName, recordDefs);
+
+        getRecordDefsByTableFromJsonStructure(item, tables, recordDefsByTable);
+      });
+    }
+  });
+};
+
+export const previewRecords = async (base: Base, json) => {
+  const tables = await findOrCreateTables(base, json);
+  let recordDefsByTable = new Map<string, Array<RecordDef>>();
+  getRecordDefsByTableFromJsonStructure(json, tables, recordDefsByTable);
+  return recordDefsByTable;
+};
+
+const BATCH_SIZE = 50;
+export const insertRecordsIntoTables = async (base: Base, json) => {
+  const tables = await findOrCreateTables(base, json);
+  let recordDefsByTable = new Map<string, Array<RecordDef>>();
+  getRecordDefsByTableFromJsonStructure(json, tables, recordDefsByTable);
+  recordDefsByTable.forEach(async (recordDefs, tableName) => {
+    const table = tables.find((table) => table.name === tableName);
+    if (table.checkPermissionsForCreateRecords()) {
+      const records = recordDefs.map((recordDef) => {
+        return {
+          fields: recordDef,
+        };
+      });
+      let i = 0;
+      while (i < records.length) {
+        const recordBatch = records.slice(i, i + BATCH_SIZE);
+        // awaiting the delete means that next batch won't be deleted until the current
+        // batch has been fully deleted, keeping you under the rate limit
+        await table.createRecordsAsync(recordBatch);
+        i += BATCH_SIZE;
+      }
+    }
+  });
+};
+
+export const insertQueryIntoQueriesTable = async (base: Base, query) => {
+  let table = base.getTableByNameIfExists("queries");
+  if (!table) {
+    if (base.unstable_hasPermissionToCreateTable()) {
+      const fields = [
+        {
+          name: "query",
+          type: FieldType.SINGLE_LINE_TEXT,
+        },
+        {
+          name: "timestamp",
+          type: FieldType.DATE_TIME,
+          options: {
+            dateFormat: {
+              name: "local",
+              format: "l",
+            },
+            timeFormat: {
+              name: "12hour",
+              format: "h:mma",
+            },
+            timeZone: "client",
+          },
+        },
+      ];
+      table = await base.unstable_createTableAsync("queries", fields);
+    }
+  }
+  if (table.checkPermissionsForCreateRecord()) {
+    table.createRecordAsync({
+      query: query,
+      timestamp: new Date(),
+    });
+  }
+};
